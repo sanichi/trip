89092364989210ed9650bf4e56277146f03080be
Mark Orr <mark.j.l.orr@googlemail.com>
2025-10-29 15:35:09 +0000

    Preparing for first http deploy

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -4,19 +4,19 @@
 # For further information see the following documentation
 # https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Content-Security-Policy
 
-Rails.application.configure do
-  config.content_security_policy do |policy|
-    policy.default_src     :none
-    policy.connect_src     :self
-    policy.img_src         :self, :data
-    policy.script_src      :self
-    policy.style_src       :self, :unsafe_inline # redcarpet
-    policy.base_uri        :self
-    policy.frame_ancestors :none
-    policy.form_action     :self
-    policy.report_uri      "https://hou.sanichi.me/reports"
-  end
-  config.content_security_policy_nonce_generator = ->(request) { request.session.id.to_s }
-  config.content_security_policy_nonce_directives = %w(script-src)
-  config.content_security_policy_report_only = false
-end
+# Rails.application.configure do
+#   config.content_security_policy do |policy|
+#     policy.default_src     :none
+#     policy.connect_src     :self
+#     policy.img_src         :self, :data
+#     policy.script_src      :self
+#     policy.style_src       :self, :unsafe_inline # redcarpet
+#     policy.base_uri        :self
+#     policy.frame_ancestors :none
+#     policy.form_action     :self
+#     policy.report_uri      "https://hou.sanichi.me/reports"
+#   end
+#   config.content_security_policy_nonce_generator = ->(request) { request.session.id.to_s }
+#   config.content_security_policy_nonce_directives = %w(script-src)
+#   config.content_security_policy_report_only = false
+# end
--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,4 +1,4 @@
 # Be sure to restart your server when you modify this file.
 Rails.application.config.session_store :cookie_store,
-  key: "#{Rails.env.production? ? '__Secure-' : '_'}Tmp-Session",
+  key: "#{Rails.env.production? && false ? '__Secure-' : '_'}Trip-Session",
   expire_after: 2.weeks
