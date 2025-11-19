ae799b691127db1892504b13051e7bc4c9f25d6e
Mark Orr <mark.j.l.orr@googlemail.com>
2025-11-19 11:41:27 +0000

    Next and previous day links

--- app/helpers/day_helper.rb
+++ app/helpers/day_helper.rb
@@ -6,4 +6,12 @@ module DayHelper
       content_tag(:span, t('symbol.tick'), class: "badge bg-success")
     end
   end
+
+  def day_previous(day)
+    day.trip.days.where("date < ?", day.date).order(date: :desc).first
+  end
+
+  def day_next(day)
+    day.trip.days.where("date > ?", day.date).order(date: :asc).first
+  end
 end
